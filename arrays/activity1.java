d4f88b26d1274c569d5b4ba5467f172b3cd621b7
Umer Farooq <umerperfect055@gmail.com>
2023-06-23 13:46:19 +0500

    Add files via upload

--- arrays/activity1.java
+++ arrays/activity1.java
@@ -3,34 +3,29 @@ import java.util.Scanner;
 public class activity1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] array = new int[10];
-        System.out.println("Enter array elements. . .");
-        for (int i = 0; i < 10; i++) {
-            array[i] = input.nextInt();
-        }
-        int largest = array[0];
-        for (int i = 0; i < 10; i++) {
-            if (array[i] > largest) {
-                largest = array[i];
+        int[][] array = new int[3][4];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter array elements of row#  " + (i + 1));
+            for (int j = 0; j < 4; j++) {
+                array[i][j] = input.nextInt();
             }
 
         }
-        int largest2;
-        if (array[0] == largest) {
-            largest2 = array[1];
-
-        } else {
-            largest2 = array[0];
-
-        }
-        for (int i = 0; i < 10; i++) {
-            if (array[i] != largest) {
-                if (array[i] > largest2) {
-                    largest2 = array[i];
-                }
+        System.out.println("The given 2D array is. . . .");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(array[i][j] + "\t");
             }
+            System.out.println();
         }
-        System.out.println("Largest= " + largest);
-        System.out.println("2nd Largest= " + largest2);
+        int sum;
+        for (int i = 0; i < 3; i++) {
+            sum = 0;
+            for (int j = 0; j < 4; j++) {
+                sum = array[i][j];
+
+            }
+            System.out.println("Sum of row " + (i + 1) + " is " + sum);
+        }
     }
 }
